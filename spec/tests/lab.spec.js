5fe4f5defe3eca12ac61cda3bf8ede081f239576
heba sabri <heba.sabri.183@gmail.com>
2025-02-23 00:30:17 +0200

    first commit

--- spec/tests/lab.spec.js
+++ spec/tests/lab.spec.js
@@ -18,7 +18,7 @@ describe("lab testing:", () => {
         it("req to get(/user/search) ,expect to get the correct user with his name", async () => {
             const res = await req.get("/user/search").query({ name: fakeUser.name });
             expect(res.status).toBe(200);
-            expect(res.body.name).toBe("ali");
+            expect(res.body.name).toBe(fakeUser.name);
         })
         it("req to get(/user/search) with invalid name ,expect res status and res message to be as expected", async () => {
             const res = await req.get("/user/search").query({ name: "InvalidUser" });
@@ -36,30 +36,31 @@ describe("lab testing:", () => {
             await req.post("/user/signup").send(fakeUser);
             let loginRes = await req.post("/user/login").send(fakeUser);
             userToken = loginRes.body.data;
-            const todoRes = await req.post("/todo").set({authorization:userToken}).send({ title: "Initial Todo" });
+            const todoRes = await req.post("/todo").set("Authorization", userToken).send({ title: "Initial Todo" });
             todoId = todoRes.body.data._id;
-        });
+        })
         afterAll(async () => {
-            await clearDatabase()
+            await clearDatabase();
         })
         it("req to patch( /todo/) with id only ,expect res status and res message to be as expected", async () => {
-            const res = await req.patch("/todo/").send({ id: todoId }).set({authorization:userToken});
+            const res = await req.patch(`/todo/${todoId}`).send({ id: todoId }).set("Authorization", userToken);
             expect(res.status).toBe(400);
             expect(res.body.message).toBe("Title is required");
         })
         it("req to patch( /todo/) with id and title ,expect res status and res to be as expected", async () => {
-            const res = await req.patch("/todo/").send({ id: todoId, title: "New Title" }).set({authorization:userToken});
+            const res = await req.patch(`/todo/${todoId}`).send({ id: todoId, title: "New Title" }).set("Authorization", userToken);
             expect(res.status).toBe(200);
-            expect(res.body.title).toBe("New Title");
+            expect(res.body.data.title).toBe("New Title");
         })
 
         it("req to get( /todo/user) ,expect to get all user's todos", async () => {
-            const res = await req.get("/todo/user").set({authorization:userToken});
+            const res = await req.get("/todo/user").set("Authorization", userToken);
             expect(res.status).toBe(200);
             expect(res.body.data.some((todo) => todo._id === todoId)).toBe(true);
         })
         it("req to get( /todo/user) ,expect to not get any todos for user hasn't any todo", async () => {
-            const res = await req.get("/todo/user").set({authorization:userToken});
+            await clearDatabase();
+            const res = await req.get("/todo/user").set("Authorization", userToken);
             expect(res.status).toBe(404);
             expect(res.body.message).toBe("No todos found");
         })
